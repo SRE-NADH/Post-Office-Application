3148b987c4bc09d3309e3b2a80f9eb3b63b6f25d
SRE-NADH <sreenadh669@gmail.com>
2023-10-04 11:17:18 +0530

    compleated

--- result/resultPage.js
+++ result/resultPage.js
@@ -10,11 +10,13 @@ const timezone = document.getElementById("timezone");
 const date = document.getElementById("date");
 const pincode = document.getElementById("pincode");
 const message = document.getElementById('message');
+const postcards = document.getElementsByClassName("post-cards")[0];
+const input = document.getElementById('search');
 
 
 let cookiedata=document.cookie.split("=")[1];
 let objdata = JSON.parse(cookiedata);
-console.log(objdata);
+//console.log(objdata);
 ipadress.innerText = " "+objdata.Ip;
 latitude.innerText= " "+objdata.Latitude;
 longitude.innerText =" "+objdata.Longitude;
@@ -37,25 +39,72 @@ async function fetchPincodes(){
     try{
         let response = await fetch(url);
         let result = await response.json();
-        addcardsToUi(result);
+        message.innerHTML=`Message: ${result[0].Message}`;
+        addcardsToUi(result[0].PostOffice);
     }
     catch(error){
         window.alert("error occured while fetching postal details");
     }
 }
+let Nameobj =[];
+let Postobj=[];
 function addcardsToUi(result){
-  console.log(result);
+  //console.log(result);
+  postcards.innerHTML="";
+  let arr = result;
+  arr.forEach(obj=>{
+     data ={};
+     data[obj.Name]=obj;
+     Nameobj.push(data);
+     data={};
+     data[obj.BranchType]=obj;
+     Nameobj.push(data);
+     
+    let card = document.createElement("div");
+    card.className = "card";
+    card.innerHTML =`<p id="name">${obj.Name}</p>
+    <p id="branchtype">${obj.BranchType}</p>
+    <p id="deliverystatus">${obj.DeliveryStatus}</p>
+    <p id="district">${obj.District}</p>
+    <p id="division">${obj.Division}</p>`;
+
+    postcards.append(card);
+  });
+//   console.log(Nameobj);
 }
 
 fetchPincodes();
 
+input.addEventListener("input",()=>{
+
+    console.log(1);
+    const filterValue = input.value.toLowerCase();
+    let arr= new Set();
+    Nameobj.forEach(item => {
+        let key = Object.keys(item)[0];
+         if(key.toLowerCase().includes(filterValue)){
+            arr.add(item[key]);
+         }
+});
+let data = [...arr]
+// console.log(data);
+ if(data.length!=0){
+    addcardsToUi(data);
+}
+});
 
 
 
-//message.innerHTML=`Message: <span>Number of pincode(s) found: ${findlength()}<span>`
 
 
 
 
 
+{/* <div class="card">
+<p id="name">name</p>
+<p id="branchtype">Branch type</p>
+<p id="deliverystatus">deliverystatus</p>
+<p id="district">district</p>
+<p id="division">divison</p>
+</div> */}
 
